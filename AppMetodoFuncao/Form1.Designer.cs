7dc23faa4f0c8dd8ee7cf9bb4363617e6b617b63
alberto-Lucas <100029506+alberto-Lucas@users.noreply.github.com>
2024-03-15 19:26:44 -0300

    Aula 08/03

---
+++ AppMetodoFuncao/Form1.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace AppMetodoFuncao
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbbOpcao = new System.Windows.Forms.ComboBox();
+            this.btnIF = new System.Windows.Forms.Button();
+            this.btnSwitch = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtV1 = new System.Windows.Forms.TextBox();
+            this.txtV2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnFuncao = new System.Windows.Forms.Button();
+            this.btnMetodo = new System.Windows.Forms.Button();
+            this.txtOperacao = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.btnMultiplicar = new System.Windows.Forms.Button();
+            this.btnSomar = new System.Windows.Forms.Button();
+            this.btnIniciar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cbbOpcao
+            // 
+            this.cbbOpcao.FormattingEnabled = true;
+            this.cbbOpcao.Items.AddRange(new object[] {
+            "Opção 1",
+            "Opção 2",
+            "Opção 3",
+            "Opção 4"});
+            this.cbbOpcao.Location = new System.Drawing.Point(12, 12);
+            this.cbbOpcao.Name = "cbbOpcao";
+            this.cbbOpcao.Size = new System.Drawing.Size(121, 21);
+            this.cbbOpcao.TabIndex = 0;
+            // 
+            // btnIF
+            // 
+            this.btnIF.Location = new System.Drawing.Point(12, 39);
+            this.btnIF.Name = "btnIF";
+            this.btnIF.Size = new System.Drawing.Size(75, 23);
+            this.btnIF.TabIndex = 1;
+            this.btnIF.Text = "IF";
+            this.btnIF.UseVisualStyleBackColor = true;
+            this.btnIF.Click += new System.EventHandler(this.btnIF_Click);
+            // 
+            // btnSwitch
+            // 
+            this.btnSwitch.Location = new System.Drawing.Point(12, 68);
+            this.btnSwitch.Name = "btnSwitch";
+            this.btnSwitch.Size = new System.Drawing.Size(75, 23);
+            this.btnSwitch.TabIndex = 2;
+            this.btnSwitch.Text = "SWITCH";
+            this.btnSwitch.UseVisualStyleBackColor = true;
+            this.btnSwitch.Click += new System.EventHandler(this.btnSwitch_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(166, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Valor1";
+            // 
+            // txtV1
+            // 
+            this.txtV1.Location = new System.Drawing.Point(169, 31);
+            this.txtV1.Name = "txtV1";
+            this.txtV1.Size = new System.Drawing.Size(100, 20);
+            this.txtV1.TabIndex = 4;
+            // 
+            // txtV2
+            // 
+            this.txtV2.Location = new System.Drawing.Point(169, 70);
+            this.txtV2.Name = "txtV2";
+            this.txtV2.Size = new System.Drawing.Size(100, 20);
+            this.txtV2.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(166, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Valor2";
+            // 
+            // btnFuncao
+            // 
+            this.btnFuncao.Location = new System.Drawing.Point(284, 60);
+            this.btnFuncao.Name = "btnFuncao";
+            this.btnFuncao.Size = new System.Drawing.Size(75, 23);
+            this.btnFuncao.TabIndex = 8;
+            this.btnFuncao.Text = "FUNCAO";
+            this.btnFuncao.UseVisualStyleBackColor = true;
+            this.btnFuncao.Click += new System.EventHandler(this.btnFuncao_Click);
+            // 
+            // btnMetodo
+            // 
+            this.btnMetodo.Location = new System.Drawing.Point(284, 31);
+            this.btnMetodo.Name = "btnMetodo";
+            this.btnMetodo.Size = new System.Drawing.Size(75, 23);
+            this.btnMetodo.TabIndex = 7;
+            this.btnMetodo.Text = "METODO";
+            this.btnMetodo.UseVisualStyleBackColor = true;
+            this.btnMetodo.Click += new System.EventHandler(this.btnMetodo_Click);
+            // 
+            // txtOperacao
+            // 
+            this.txtOperacao.Location = new System.Drawing.Point(169, 115);
+            this.txtOperacao.Name = "txtOperacao";
+            this.txtOperacao.Size = new System.Drawing.Size(100, 20);
+            this.txtOperacao.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(166, 99);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Operação";
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Location = new System.Drawing.Point(169, 160);
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.Size = new System.Drawing.Size(100, 20);
+            this.txtResultado.TabIndex = 12;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(166, 144);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Resultado";
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(284, 94);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 13;
+            this.btnCalcular.Text = "CALCULAR";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // btnMultiplicar
+            // 
+            this.btnMultiplicar.Location = new System.Drawing.Point(284, 160);
+            this.btnMultiplicar.Name = "btnMultiplicar";
+            this.btnMultiplicar.Size = new System.Drawing.Size(96, 23);
+            this.btnMultiplicar.TabIndex = 14;
+            this.btnMultiplicar.Text = "MULTIPLICAR";
+            this.btnMultiplicar.UseVisualStyleBackColor = true;
+            this.btnMultiplicar.Click += new System.EventHandler(this.btnMultiplicar_Click);
+            // 
+            // btnSomar
+            // 
+            this.btnSomar.Location = new System.Drawing.Point(284, 123);
+            this.btnSomar.Name = "btnSomar";
+            this.btnSomar.Size = new System.Drawing.Size(75, 23);
+            this.btnSomar.TabIndex = 15;
+            this.btnSomar.Text = "SOMAR";
+            this.btnSomar.UseVisualStyleBackColor = true;
+            this.btnSomar.Click += new System.EventHandler(this.btnSomar_Click);
+            // 
+            // btnIniciar
+            // 
+            this.btnIniciar.Location = new System.Drawing.Point(414, 89);
+            this.btnIniciar.Name = "btnIniciar";
+            this.btnIniciar.Size = new System.Drawing.Size(96, 23);
+            this.btnIniciar.TabIndex = 16;
+            this.btnIniciar.Text = "INICIAR";
+            this.btnIniciar.UseVisualStyleBackColor = true;
+            this.btnIniciar.Click += new System.EventHandler(this.btnIniciar_Click);
+            this.btnIniciar.MouseEnter += new System.EventHandler(this.btnIniciar_MouseEnter);
+            this.btnIniciar.MouseLeave += new System.EventHandler(this.btnIniciar_MouseLeave);
+            this.btnIniciar.MouseHover += new System.EventHandler(this.btnIniciar_MouseHover);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(649, 300);
+            this.Controls.Add(this.btnIniciar);
+            this.Controls.Add(this.btnSomar);
+            this.Controls.Add(this.btnMultiplicar);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtOperacao);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnFuncao);
+            this.Controls.Add(this.btnMetodo);
+            this.Controls.Add(this.txtV2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtV1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSwitch);
+            this.Controls.Add(this.btnIF);
+            this.Controls.Add(this.cbbOpcao);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbbOpcao;
+        private System.Windows.Forms.Button btnIF;
+        private System.Windows.Forms.Button btnSwitch;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtV1;
+        private System.Windows.Forms.TextBox txtV2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnFuncao;
+        private System.Windows.Forms.Button btnMetodo;
+        private System.Windows.Forms.TextBox txtOperacao;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtResultado;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Button btnMultiplicar;
+        private System.Windows.Forms.Button btnSomar;
+        private System.Windows.Forms.Button btnIniciar;
+    }
+}
+
---
+++ AppMetodoFuncao/Form1.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppMetodoFuncao
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIF_Click(object sender, EventArgs e)
+        {
+            string opcao;
+
+            if (cbbOpcao.SelectedIndex == 0)
+                opcao = "Primeiro Iten selecionado";
+            else if(cbbOpcao.SelectedIndex == 1)
+                opcao = "Segundo Iten selecionado";
+            else if (cbbOpcao.SelectedIndex == 2)
+                opcao = "Terceiro Iten selecionado";
+            else if (cbbOpcao.SelectedIndex == 3)
+                opcao = "Quarto Iten selecionado";
+            else
+                opcao = "Nenhum selecinado";
+
+            MessageBox.Show(opcao);
+        }
+
+        private void btnSwitch_Click(object sender, EventArgs e)
+        {
+            string opcao;
+
+            switch(cbbOpcao.SelectedIndex)
+            {
+                case 0:
+                    opcao = "Primeiro Item";
+                    break;
+                case 1:
+                    opcao = "Segundo Item";
+                    break;
+                case 2:
+                    opcao = "Terceiro Item";
+                    break;
+                case 3:
+                    opcao = "Quarto Item";
+                    break;
+                default:
+                    opcao = "Nenhum item";
+                    break;
+            }
+
+            MessageBox.Show(opcao);
+        }
+
+        private void btnMetodo_Click(object sender, EventArgs e)
+        {
+            MetodoSomar();
+        }
+
+        //Método
+        //Void - não retorna nada
+        void MetodoSomar()
+        {
+            int v1, v2, resultado;
+
+            v1 = int.Parse(txtV1.Text);
+            v2 = int.Parse(txtV2.Text);
+
+            resultado = v1 + v2;
+
+            MessageBox.Show("O resulta da soma é: " +
+                resultado.ToString());
+        }
+
+        //Função
+        //int define o tipo do retorno
+        //obrigatorio usa da palavra return
+        int FuncaoSomar()
+        {
+            int v1, v2;
+
+            v1 = int.Parse(txtV1.Text);
+            v2 = int.Parse(txtV2.Text);
+
+            return v1 + v2;
+        }
+
+        private void btnFuncao_Click(object sender, EventArgs e)
+        {
+            int resultado;
+            resultado = FuncaoSomar();
+
+            MessageBox.Show("O resultado é: " + resultado);
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            txtResultado.Text =
+                Calcular(
+                            int.Parse(txtV1.Text),
+                            int.Parse(txtV2.Text),
+                            txtOperacao.Text
+                        ).ToString();
+        }
+
+        int Calcular(int v1, int v2, string operacao)
+        {
+            switch(operacao)
+            {
+                case "+": return v1 + v2;
+                case "-": return v1 - v2;
+                case "*": return v1 * v2;
+                case "/": return v1 / v2;
+                default: return 0;
+            }
+        }
+
+        private void btnMultiplicar_Click(object sender, EventArgs e)
+        {
+            txtResultado.Text =
+                Calcular(2, 2, "*").ToString();
+        }
+
+        private void btnSomar_Click(object sender, EventArgs e)
+        {
+            txtResultado.Text = 
+            Calcular(int.Parse(txtV1.Text),
+                int.Parse(txtV2.Text),
+                "+").ToString();
+        }
+
+        private void btnIniciar_Click(object sender, EventArgs e)
+        {
+            bool resposta;
+
+            resposta =
+                MessageBox.Show(
+                "O Lucas é o melhor professor?",
+                "Pergunta",
+                MessageBoxButtons.YesNo) == 
+                DialogResult.Yes;
+
+            while(!resposta)
+            {
+                resposta =
+                MessageBox.Show(
+                "O Lucas é o melhor professor?",
+                "Pergunta",
+                MessageBoxButtons.YesNo) ==
+                DialogResult.Yes;
+            }
+        }
+
+        private void btnIniciar_MouseHover(object sender, EventArgs e)
+        {
+            MoverBotao();
+        }
+
+        void MoverBotao()
+        {
+            Random randon = new Random();
+            int x, y;
+
+            x = randon.Next(0, this.Width - 30);
+            y = randon.Next(0, this.Height - 30);
+
+            btnIniciar.Left = x;
+            btnIniciar.Top = y;
+        }
+
+        private void btnIniciar_MouseLeave(object sender, EventArgs e)
+        {
+            MoverBotao();
+        }
+
+        private void btnIniciar_MouseEnter(object sender, EventArgs e)
+        {
+            MoverBotao();
+        }
+    }
+}
