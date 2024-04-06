d26034c5f3928d3ffc395809bc5f90d9bdfc8c3f
alberto-Lucas <100029506+alberto-Lucas@users.noreply.github.com>
2024-04-05 22:27:09 -0300

    AppCadastros 05/04/2024

---
+++ AppCadastros/frmCadCliente.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppCadastros
+{
+    public partial class frmCadCliente : Form
+    {
+        public frmCadCliente()
+        {
+            InitializeComponent();
+
+            //Iremos desativar a validação
+            //automatica
+            AutoValidate = AutoValidate.Disable;
+        }
+
+        private void txtNome_Validating(object sender, CancelEventArgs e)
+        {
+            //Neste iremos validar o campo
+            //txtNome
+
+            //Validar se o campo está preenchido
+            if(string.IsNullOrEmpty(txtNome.Text))
+            { 
+                //Caso o campo esteja vazio
+                //sera cancela a operação da tela
+                //Ou seja irei cancelar o processo
+                //de cadastro do registro
+                e.Cancel = true;
+
+                //Definir a mensagem a ser exibida
+                //Vincular o campo a mensagem
+                errorProvider1.SetError(
+                    txtNome, "Preencha o Nome.");
+            }
+            else
+            {
+                //Aqui resetemos a notificação
+                //de erro
+
+                //Cancelar o cancelamento
+                e.Cancel = false;
+
+                //Resetar a mensagem
+                errorProvider1.SetError(
+                    txtNome, "");
+            }
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            //Validando se possui
+            //alguma notificação configurada
+            //Se possuir um cancelamento do cancelamento
+            //Esta tudo ok
+            //Se tiver cancelamento ativo,
+            //temos um problema
+            if(!ValidateChildren(
+                ValidationConstraints.Enabled))
+            {
+                MessageBox.Show(
+                    "É nescessario o preenchimento" +
+                    "de todos os campos, obrigatorios",
+                    "Atenção",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning);
+            }
+            else
+            {
+                MessageBox.Show(
+                    "Registro salvo com sucesso!",
+                    "Informação",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information);
+            }
+        }
+    }
+}
---
+++ AppCadastros/frmPrincipal.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppCadastros
+{
+    public partial class frmPrincipal : Form
+    {
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void sairToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //Encerrar a aplicação por completo
+            //Utilizar apenas na tela
+            //Principal
+            Application.Exit();
+
+            //Encerro apenas o Form
+            //Utilizar apenas nos 
+            //Formularios Filhos
+            //Close();
+        }
+
+        private void frmPrincipal_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            //Neste ao fechar a tela
+            //Sera realizada uma mensagem 
+            //de confirmação de fechamento
+            //para o usuário
+            if (MessageBox.Show(
+                "Deseja realmente sair?", //Mensagem
+                "Atenção!", //Titulo Mensagem
+                MessageBoxButtons.YesNo, //Defino o botões
+                MessageBoxIcon.Warning, //Icone da Mensagem
+                MessageBoxDefaultButton.Button2 //Botão Padrão
+                ) == DialogResult.No)
+            {
+                //Aqui no evento FormClossing
+                //Precisamos cancelar o fechamento
+                e.Cancel = true;
+            }
+        }
+
+        private void clientesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //Criar a chamada de uma nova tela
+            //Primeiro é preciso carregar
+            //a tela em memoria
+
+            //Utilizando a palavra new
+            //para criar o objeto em memoria
+
+            //Definir o que sera chamado
+            //Definir a variavel que
+            //recebera a instancia
+            //Criamos a instancia em memoria
+
+            frmCadCliente form =
+                new frmCadCliente();
+
+            //Utilizando a variavel form
+            //iremos apresentar a tela 
+            //para o usuario
+
+            //Sera apresentado uma tela separada
+            //da tela principal
+            //podendo manipular a tela mae 
+            //separa da tela filha
+            //form.Show();
+
+
+            //A tela é apresentada vinculada
+            //a tela mae
+            //portanto não é possivel mexer 
+            //na tela mae, equanto a filha estiver
+            //sendo apresentada.
+            form.ShowDialog();
+        }
+    }
+}
