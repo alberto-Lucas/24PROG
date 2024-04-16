8f68e7c00de2b906d439cda5175d6f5e3c1073c5
Lucas Alberto <lucas.alberto@linx.com>
2024-04-16 16:02:44 -0300

    AppManipularArquivo

---
+++ AppManipulaArquivo/Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace AppManipulaArquivo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtCaminho = new System.Windows.Forms.TextBox();
+            this.btnSelecionarCaminho = new System.Windows.Forms.Button();
+            this.txtNomeArquivo = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnCriarArquivo = new System.Windows.Forms.Button();
+            this.btnGravar = new System.Windows.Forms.Button();
+            this.txtTexto = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnCarregar = new System.Windows.Forms.Button();
+            this.txtArquivo = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnAdicionar = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.btnCarregarTodos = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Caminho:";
+            // 
+            // txtCaminho
+            // 
+            this.txtCaminho.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtCaminho.Location = new System.Drawing.Point(12, 25);
+            this.txtCaminho.Name = "txtCaminho";
+            this.txtCaminho.Size = new System.Drawing.Size(308, 20);
+            this.txtCaminho.TabIndex = 1;
+            // 
+            // btnSelecionarCaminho
+            // 
+            this.btnSelecionarCaminho.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSelecionarCaminho.Location = new System.Drawing.Point(326, 23);
+            this.btnSelecionarCaminho.Name = "btnSelecionarCaminho";
+            this.btnSelecionarCaminho.Size = new System.Drawing.Size(75, 23);
+            this.btnSelecionarCaminho.TabIndex = 2;
+            this.btnSelecionarCaminho.Text = "Selecionar";
+            this.btnSelecionarCaminho.UseVisualStyleBackColor = true;
+            this.btnSelecionarCaminho.Click += new System.EventHandler(this.btnSelecionarCaminho_Click);
+            // 
+            // txtNomeArquivo
+            // 
+            this.txtNomeArquivo.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtNomeArquivo.Location = new System.Drawing.Point(12, 69);
+            this.txtNomeArquivo.Name = "txtNomeArquivo";
+            this.txtNomeArquivo.Size = new System.Drawing.Size(308, 20);
+            this.txtNomeArquivo.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Nome Arquivo:";
+            // 
+            // btnCriarArquivo
+            // 
+            this.btnCriarArquivo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCriarArquivo.Location = new System.Drawing.Point(326, 67);
+            this.btnCriarArquivo.Name = "btnCriarArquivo";
+            this.btnCriarArquivo.Size = new System.Drawing.Size(75, 23);
+            this.btnCriarArquivo.TabIndex = 5;
+            this.btnCriarArquivo.Text = "Criar";
+            this.btnCriarArquivo.UseVisualStyleBackColor = true;
+            this.btnCriarArquivo.Click += new System.EventHandler(this.btnCriarArquivo_Click);
+            // 
+            // btnGravar
+            // 
+            this.btnGravar.Location = new System.Drawing.Point(12, 291);
+            this.btnGravar.Name = "btnGravar";
+            this.btnGravar.Size = new System.Drawing.Size(75, 23);
+            this.btnGravar.TabIndex = 8;
+            this.btnGravar.Text = "Gravar";
+            this.btnGravar.UseVisualStyleBackColor = true;
+            this.btnGravar.Click += new System.EventHandler(this.btnGravar_Click);
+            // 
+            // txtTexto
+            // 
+            this.txtTexto.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtTexto.Location = new System.Drawing.Point(12, 121);
+            this.txtTexto.Multiline = true;
+            this.txtTexto.Name = "txtTexto";
+            this.txtTexto.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtTexto.Size = new System.Drawing.Size(389, 164);
+            this.txtTexto.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 105);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Texto:";
+            // 
+            // btnCarregar
+            // 
+            this.btnCarregar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnCarregar.Location = new System.Drawing.Point(12, 516);
+            this.btnCarregar.Name = "btnCarregar";
+            this.btnCarregar.Size = new System.Drawing.Size(75, 23);
+            this.btnCarregar.TabIndex = 11;
+            this.btnCarregar.Text = "Carregar";
+            this.btnCarregar.UseVisualStyleBackColor = true;
+            this.btnCarregar.Click += new System.EventHandler(this.btnCarregar_Click);
+            // 
+            // txtArquivo
+            // 
+            this.txtArquivo.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtArquivo.Location = new System.Drawing.Point(12, 346);
+            this.txtArquivo.Multiline = true;
+            this.txtArquivo.Name = "txtArquivo";
+            this.txtArquivo.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtArquivo.Size = new System.Drawing.Size(389, 164);
+            this.txtArquivo.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 330);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Arquivo:";
+            // 
+            // btnAdicionar
+            // 
+            this.btnAdicionar.Location = new System.Drawing.Point(93, 291);
+            this.btnAdicionar.Name = "btnAdicionar";
+            this.btnAdicionar.Size = new System.Drawing.Size(75, 23);
+            this.btnAdicionar.TabIndex = 12;
+            this.btnAdicionar.Text = "Adicionar";
+            this.btnAdicionar.UseVisualStyleBackColor = true;
+            this.btnAdicionar.Click += new System.EventHandler(this.btnAdicionar_Click);
+            // 
+            // btnCarregarTodos
+            // 
+            this.btnCarregarTodos.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnCarregarTodos.Location = new System.Drawing.Point(93, 516);
+            this.btnCarregarTodos.Name = "btnCarregarTodos";
+            this.btnCarregarTodos.Size = new System.Drawing.Size(75, 23);
+            this.btnCarregarTodos.TabIndex = 13;
+            this.btnCarregarTodos.Text = "Carregar Todos";
+            this.btnCarregarTodos.UseVisualStyleBackColor = true;
+            this.btnCarregarTodos.Click += new System.EventHandler(this.btnCarregarTodos_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(413, 549);
+            this.Controls.Add(this.btnCarregarTodos);
+            this.Controls.Add(this.btnAdicionar);
+            this.Controls.Add(this.btnCarregar);
+            this.Controls.Add(this.txtArquivo);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnGravar);
+            this.Controls.Add(this.txtTexto);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnCriarArquivo);
+            this.Controls.Add(this.txtNomeArquivo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnSelecionarCaminho);
+            this.Controls.Add(this.txtCaminho);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Manipular Arquivo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtCaminho;
+        private System.Windows.Forms.Button btnSelecionarCaminho;
+        private System.Windows.Forms.TextBox txtNomeArquivo;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnCriarArquivo;
+        private System.Windows.Forms.Button btnGravar;
+        private System.Windows.Forms.TextBox txtTexto;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnCarregar;
+        private System.Windows.Forms.TextBox txtArquivo;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnAdicionar;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button btnCarregarTodos;
+    }
+}
+
---
+++ AppManipulaArquivo/Form1.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace AppManipulaArquivo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSelecionarCaminho_Click(object sender, EventArgs e)
+        {
+            //Utilizando o componenete folderBrowserDialog1
+            //Iremos apresentar a tela de seleção de pastas
+            //para o usuario e iremos coletar o resutado 
+            //da seleção
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            //Verificamos se foi clicar no botão ok ou cancelar
+            //se clicado no cancelar iremos ignorar
+            //se clicado no ok, iremos validar o diretorio
+            //selecionado
+            //se tudo ok, iremos apresentar o diretorio
+            //ao usuario no textbox
+            if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(folderBrowserDialog1.SelectedPath))
+            {
+                txtCaminho.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        //Função para validar diretorio
+        bool ValidarDiretorio()
+        {
+            //Iremos recuperar os dados informados pelo usuario
+            string diretorio = txtCaminho.Text;
+            string nomeArquivo = txtNomeArquivo.Text;
+
+            //Validamos o preenchimento dos campos
+            if (string.IsNullOrWhiteSpace(diretorio) || string.IsNullOrWhiteSpace(nomeArquivo))
+            {
+                MessageBox.Show("Por favor, selecione um diretório e informe um nome de arquivo.");
+                //Utilizamso o return para encerrar o método
+                //visto que se os campos não forem validos
+                //não tem como prosseguir
+                //então podemos encerrar
+                return false;
+            }
+
+            //Validamos se o diretorio é valido
+            if (!Directory.Exists(diretorio))
+            {
+                MessageBox.Show("O diretório especificado não existe.");
+                return false;
+            }
+
+            return true;
+        }
+
+        //Função para retorno o diretorio completo
+        string GetDiretorioCompleto()
+        {
+            //Diretorio + Nome arquivo
+            return Path.Combine(txtCaminho.Text, txtNomeArquivo.Text + ".txt");
+        }
+
+
+        private void btnCriarArquivo_Click(object sender, EventArgs e)
+        {
+            //Validamos os dados do diretorio
+            if (!ValidarDiretorio())
+                return;
+
+            //Recuperamos o diretorio completo
+            string caminhoCompleto = GetDiretorioCompleto();
+
+            //utilizamos o Try Catch para criação do arquivo
+            //pois caso ocorra uma falha iremos tratar
+            //Pode ocorrer falha de permissão da pasta
+            try
+            {
+                //Verificação se o arquivo existe no diretorio
+                if (!File.Exists(caminhoCompleto))
+                {
+                    //Se não existir criamos um arquivo vazios
+                    File.WriteAllText(caminhoCompleto, "");
+                    //E notificamos o usuario
+                    MessageBox.Show("Arquivo criado com sucesso!");
+                }
+                else
+                {
+                    //Se ja existir, notificamos o usuario
+                    MessageBox.Show("O arquivo já existe.");
+                }
+            }
+            catch (Exception ex)
+            {
+                //Caso ocorra uma falha, notificamos o usuario
+                //Só ira notificar caso ocorra algum erro na execução
+                MessageBox.Show("Ocorreu um erro ao criar o arquivo: " + ex.Message);
+            }
+        }
+
+        private void btnGravar_Click(object sender, EventArgs e)
+        {
+            //Validamos os dados do diretorio
+            if (!ValidarDiretorio())
+                return;
+
+            //Obtém o caminho completo do arquivo
+            string caminhoCompleto = GetDiretorioCompleto();
+
+            try
+            {
+                //Grava o texto do TextBox no arquivo
+                //Todo o conteudo do arquivo será subistituido
+                //Utiliazado Environment.NewLine para gerar uma nova linha no final do arquivo
+                File.WriteAllText(caminhoCompleto, txtTexto.Text + Environment.NewLine);
+                MessageBox.Show("Dados gravados com sucesso!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ocorreu um erro ao gravar os dados no arquivo: " + ex.Message);
+            }
+        }
+
+        private void btnAdicionar_Click(object sender, EventArgs e)
+        {
+            //Validamos os dados do diretorio
+            if (!ValidarDiretorio())
+                return;
+
+            //Obtém o caminho completo do arquivo
+            string caminhoCompleto = GetDiretorioCompleto();
+
+            try
+            {
+                //Grava o texto do TextBox no arquivo
+                //Será adicionado o texto ao arquivo
+                //Utiliazado Environment.NewLine para gerar uma nova linha no final do arquivo
+                File.AppendAllText(caminhoCompleto, txtTexto.Text + Environment.NewLine);
+                MessageBox.Show("Dados gravados com sucesso!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ocorreu um erro ao gravar os dados no arquivo: " + ex.Message);
+            }
+        }
+
+        private void btnCarregar_Click(object sender, EventArgs e)
+        {
+            // Verifica se o diretório e o nome do arquivo são válidos
+            if (!ValidarDiretorio())
+                return;
+
+            // Obtém o caminho completo do arquivo
+            string caminhoCompleto = GetDiretorioCompleto();
+
+            try
+            {
+                // Verifica se o arquivo existe
+                if (File.Exists(caminhoCompleto))
+                {
+                    // Lê todo o texto do arquivo e o exibe no TextBox
+                    string conteudo = File.ReadAllText(caminhoCompleto);
+                    txtArquivo.Text = conteudo;
+                    MessageBox.Show("Conteúdo carregado com sucesso!");
+                }
+                else
+                {
+                    MessageBox.Show("O arquivo não existe.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ocorreu um erro ao carregar o arquivo: " + ex.Message);
+            }
+        }
+
+        private void btnCarregarTodos_Click(object sender, EventArgs e)
+        {
+            //Limpar o txtArquivo
+            txtArquivo.Clear();
+
+            try
+            {
+                //Obtém todos os arquivos .txt do diretorio
+                //E armazena cada arquivo em um array de diretorios
+                string[] diretorios = Directory.GetFiles(txtCaminho.Text, "*.txt");
+
+                //Realiza um loop, passadno por cada aquivo
+                //e lendo seu conteudo
+                foreach (string arquivo in diretorios)
+                {
+                    // Lê o conteúdo do arquivo
+                    string conteudo = File.ReadAllText(arquivo);
+
+                    // Exibe o conteúdo em um TextBox multiline
+                    txtArquivo.AppendText("Conteúdo do arquivo: " + Path.GetFileName(arquivo) + Environment.NewLine);
+                    txtArquivo.AppendText(conteudo + Environment.NewLine);
+                    txtArquivo.AppendText("------------------------------------" + Environment.NewLine);
+                }
+
+                MessageBox.Show("Todos os arquivos .txt foram lidos com sucesso!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ocorreu um erro ao ler os arquivos .txt: " + ex.Message);
+            }
+        }
+    }
+}
